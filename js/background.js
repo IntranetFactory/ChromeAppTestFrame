05077a78d510cd39b88bcb77e13d20bf078d3dd3
Ivan Jovanovic <ij.smartnet@gmail.com>
2016-03-01 11:19:05 +0100

    AT-102 wrote detailed comments in code

--- js/background.js
+++ js/background.js
@@ -1,25 +1,68 @@
+/**
+* Chrome extensions have background page available to them for use
+* Basically, browser creates a html document with window and document objects, loads html, css and javascript but doesn't display that document
+* The purpose of the background page is to exist in memory in the background
+* It is important to know that background page state is shared between extention popup window and any tab(s) opened in the browser
+*
+* To learn more about background pages read https://developer.chrome.com/extensions/background_pages
+*
+* Popup page can communicate with background page in two ways
+* 1. chrome.extension.getBackgroundPage()
+* 2. extension.runtime
+*
+* chrome.extension.getBackgroundPage() function returns window object of the background page. Any and all global variables set on window object of the background page
+* are available for use in popup page
+*
+* extension.runtime offers infrastructure for client-server message passing communication between popup page and background page via concept of ports
+* port is a two-way communication channel between holders of the handle to the port object
+*
+* To use this infrastructure do the following:
+* 1. in the background page attach a listener to the chrome.extension.onConnect event. This event will be triggered each time a connection is requested with the extension
+* 2. in the popup page call chrome.extension.connect() function. This function will return the handle to the port object. Also, the same handle will be passed to the onConnect event handler in 1.
+* 3. now background page and popup page have a handle to the same port and can pass messages to each other
+*
+* To learn more about chrome.runtime read https://developer.chrome.com/extensions/runtime
+*
+* Port has onMessage event. This event is triggered each time a message is sent via port
+* Port has postMessage function. This function is used to send a message via port
+*
+* To learn more about ports read https://developer.chrome.com/extensions/runtime#type-Port
+*
+* To communicate with the background page from a page displayed in a tab specify content_scripts property in the extension manifest.
+* Content script is a js file which is loaded on a page which is opened in a tab.
+* Now, please switch to content.js file and read comments there.
+*
+* To learn more about the manifest read https://developer.chrome.com/extensions/manifest
+* To learn more about the content scripts read https://developer.chrome.com/extensions/content_scripts
+*/
+
 (function() {
   'use strict';
+  // if appController object is already created return
   if (window.appController !== undefined) {
     return;
   }
-
+  // create appController object
   window.appController = AppController();
 
   function AppController() {
+    // appController is a shared object between all open tabs
+    // we need to maintain port mapping so we know to which tab we should send the message
     var portMapping = {};
-    var currentPort = false;
 
+    // listen to onConnect event
     chrome.extension.onConnect.addListener(function(incommingPort) {
+      // get the name of the port and add the port to the portMapping
       var portName = incommingPort.name;
       if (portMapping[portName] === undefined) {
         portMapping[portName] = incommingPort;
-
+        // listen to onDisconnect event on port so we can remove it from the mapping
         incommingPort.onDisconnect.addListener(onPortDisconnected);
       }
     });
 
     function onPortDisconnected(incommingPort) {
+      // when port disconnects (ie. tab is closed) we remove the port from the port mapping
       var portName = incommingPort.name;
       if (portMapping[portName] !== undefined) {
         portMapping[portName] = undefined;
@@ -27,12 +70,17 @@
     }
 
     return {
+      // this function is used to send the message to toggle the side panel on the page
+      // the url of the page is passed as parameter
       toggleSidePanel: function(url) {
+        // calculate SHA1 hash of the url
         var hash = CryptoJS.SHA1(url);
         var hashStr = hash.toString();
         var port;
+        // use calculated hash to get the port from port mapping
         if (portMapping[hashStr] !== undefined) {
           port = portMapping[hashStr];
+          // send the message to the content script to toggle the side panel
           port.postMessage({
             signal: "ToggleSidePanel"
           });
@@ -40,9 +88,12 @@
       }
     };
   }
-
+  // browserAction is the extension button in chrome browser
+  // we attach to onClicked event of the button because we want to toggle the side panel when this button is clicked
   chrome.browserAction.onClicked.addListener(function(tab) {
+    // get the url of the tab
     var url = tab.url;
+    // toggle the side panel
     appController.toggleSidePanel(url);
   });
 }());
--- js/content.js
+++ js/content.js
@@ -1,36 +1,66 @@
+/**
+ * To communicate with the background page from a page displayed in a tab do the following
+ * 1. setup message passing infrastructure on the background page: listen to onConnect event on extension object and other events if necessary
+ * 2. specify content_scripts property in the extension manifest. content_scripts property specifies .js files which are loaded on a page which is opened in a tab.
+ * 3. in the content script use chrome.extension.runtime to make a connection with the extension
+ * 4. send and receive messages
+ *
+ * To connect to the background page from a content script
+ * 1. find out what is the extension id of the extension you want to connect to. If no id is provided, by default a connection will be established with the extension which holds the content script connection is made from
+ * 2. provide a unique name for the connection. Since extension state is shared between all the pages having a unique name is usefull for identifying connections from different pages
+ * 3. use chrome.runtime.connect function, pass extension id and unique name as parameters; this will trigger onConnect event in background page and pass the port as event handler parameter
+ * 4. a handle to the Port object will be returned as a result
+ * 5. Use Port's postMessage function to send messages
+ * 6. add a listener to the port's onMessage event to listen to incoming messages
+ *
+ * Read comments in background.js file for a more complete picture.
+ *
+ * To learn more about the manifest read https://developer.chrome.com/extensions/manifest
+ * To learn more about the content scripts read https://developer.chrome.com/extensions/content_scripts
+ */
 (function() {
   'use strict';
+  // a unique identifier is needed to identify the tab/page this content.js is loaded into
+  // href is a good choice
+  // to emphasize uniqueness a SHA1 hash is calculated from href
   var href = window.location.href;
   var hash = CryptoJS.SHA1(href);
   var hashStr = hash.toString();
 
+  // each extension has a unique id; this string is copy/pasted from settings -> extensions page
   var EXTENSION_ID = 'ghmdepaiobfijajcpfifgmcjpimmiocf';
+  // populate connectionInfo object with unique hash name created above
   var connectionInfo = {
     name: hashStr
   };
 
+  // we use chrome local storage to hold url that will be displayed in the iFrame
   var storage = chrome.storage.local;
+  // we initialize urlFrame to false; this is a convinient initial state for toggling iFrame functionality
   var urlFrame = false;
 
+  // establish the connection with the extension; this connects to the background page (.js) which is what we want
   var port = chrome.runtime.connect(EXTENSION_ID, connectionInfo);
-
+  // listen to onDisconnect event
   port.onDisconnect.addListener(onDisconnected);
-
+  // listen to onMessage event
   port.onMessage.addListener(onMessageReceived);
 
   function onDisconnected(port) {
-    // debugger;
-    // return true;
+    // for now just log the event
     console.log('Port' + port.name + ' disconnected');
   }
 
   function onMessageReceived(message, sender, sendResponseCallback) {
-    // debugger;
+    // for now log the event
     console.log('Message received ' + JSON.stringify(message));
-
     if (urlFrame) {
+      // if urlFrame is not false close side panel
       closeSidePanel(urlFrame);
+      // set urlFrame to false
+      urlFrame = false;
     } else {
+      // if urlFrame is false open side panel
       openSidePanel();
     }
   }
@@ -40,23 +70,26 @@
   // use port.postMessage to send a message
 
   function openSidePanel() {
+    // storage.get function is used to get the frameUrl from local storage
     storage.get('frameUrl', function(data) {
       if (data && data.frameUrl) {
+        // we create the iFrame, set attributes
         urlFrame = document.createElement('iframe');
-        // urlFrame.setAttribute("id", 'catFrame_sidebar');
         urlFrame.setAttribute("src", data.frameUrl);
         urlFrame.setAttribute("style", "z-index: 999999999999999; position: fixed; top: 0px; right: 0px; bottom: 0px; width: 300px; height: 100%; border:0; border-left: 1px solid #eee; box-shadow: 0px -1px 7px 0px #aaa; overflow-x: hidden;");
         urlFrame.setAttribute("allowtransparency", "false");
         urlFrame.setAttribute("scrolling", "no");
+        // and attach it to the documentElement
         document.documentElement.appendChild(urlFrame);
       }
     });
   }
 
   function closeSidePanel() {
-    document.documentElement.removeChild(urlFrame);
-    urlFrame = false;
+    // to close side panel we remove the urlFrame from the DOM
+    if (urlFrame) {
+      document.documentElement.removeChild(urlFrame);
+    }
   }
-
 
 }());
